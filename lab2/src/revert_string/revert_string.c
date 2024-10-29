e92569fa882f3504cc6fc7df4393566bd7e40728
svetlana7474 <svetlanaanisimova2004@gmail.com>
2024-10-29 11:27:15 +0000

    Ваше сообщение о коммите

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,13 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+
 }
 
